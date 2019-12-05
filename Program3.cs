9edc60537b0833953e1fffa228ad9ccf932b4436
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-12-04 22:43:05 -0800

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            //request User input with file name, save it as a string, and open the file.
+            Console.WriteLine("Enter file name");
+            string input = Console.ReadLine();
+            string text = System.IO.File.ReadAllText(input);
+            Console.WriteLine(text);
+
+
+
+        }
+    }
+}
