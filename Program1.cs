6821d740f40287d27239466bb78ed4291663c291
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-12-04 22:31:24 -0800

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            //Hello World. Calls the Writeline function to display content to the command line.
+            string input;            Console.WriteLine("Please enter some input");
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
