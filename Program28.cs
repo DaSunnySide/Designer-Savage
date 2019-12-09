8c483b2ed0fdc1b20ad4ffe2d6214a044594637d
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-12-08 18:40:56 -0800

    Create Program28.cs

---
+++ Program28.cs
@@ -0,0 +1,34 @@
+//Calculate all the possible permutations of strings
+
+ArrayList<String> printPerms(String s) {
+  ArrayList<String> result = new ArrayList<String>();
+  HashMap<Character, Integer> map = buildFreqTable(s);
+  printPerms(map, "", s.length, result);
+  return result;
+}
+
+HashMap<Character, Integer> buildFreqTable(Strings) {
+  HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+  for(char c: s.toCharArray()) {
+    if(!map.containsKey(c)) {
+      map.put(c, 0);
+    }
+    map.put(c, map.get(c)+1);
+    }
+  return map;
+}
+void printPerms(HashMap<Character, Integer> map, String prefix, int remaining, ArrayList<String> result) {
+  if(remaining == 0) {
+    result.add(prefix);
+    return;
+  }
+  
+  for(Character c: map.keySet()) {
+    int count = map.get(c);
+    if(count > 0) {
+      map.put(c, count -1 );
+      printPerms(map, prefix+c, remaining-1, result);
+      map.put(c, count);
+      }
+    }
+  }
